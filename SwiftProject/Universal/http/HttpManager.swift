016b9a7bb9157d1fab4957d366f4ff522a96824b
cloud <>
2025-08-19 09:11:37 +0800

    支持 数组数据上传

--- SwiftProject/Universal/http/HttpManager.swift
+++ SwiftProject/Universal/http/HttpManager.swift
@@ -95,7 +95,62 @@ class HttpManager: NSObject {
     }
     
     class func cancelAllRequests()-> () {
-          Alamofire.AF.cancelAllRequests()
+        Alamofire.AF.cancelAllRequests()
     }
 
+}
+//MARK: 支持数组格式的数据上传
+private let ArrayParametersKey = "ArrayParametersKey"
+ 
+/// Extenstion that allows an array be sent as a request parameters
+extension Array {
+    /// Convert the receiver array to a `Parameters` object.
+    func asParameters() -> Parameters {
+        return [ArrayParametersKey: self]
+    }
+}
+ 
+ 
+/// Convert the parameters into a json array, and it is added as the request body.
+/// The array must be sent as parameters using its `asParameters` method.
+public struct ArrayEncoding: ParameterEncoding {
+ 
+    /// The options for writing the parameters as JSON data.
+    public let options: JSONSerialization.WritingOptions
+ 
+ 
+    /// Creates a new instance of the encoding using the given options
+    ///
+    /// - parameter options: The options used to encode the json. Default is `[]`
+    ///
+    /// - returns: The new instance
+    public init(options: JSONSerialization.WritingOptions = []) {
+        self.options = options
+    }
+ 
+    public func encode(_ urlRequest: URLRequestConvertible, with parameters: Parameters?) throws -> URLRequest {
+        var urlRequest = try urlRequest.asURLRequest()
+ 
+        guard let parameters = parameters,
+            let array = parameters[ArrayParametersKey] else {
+                return urlRequest
+        }
+ 
+        do {
+            let data = try JSONSerialization.data(withJSONObject: array, options: options)
+            if urlRequest.headers["Content-Type"] == nil {
+                urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            }
+
+            urlRequest.setValue(S_UserInfoLocal.sharedInstance.userInfoModel?.token ?? "", forHTTPHeaderField: "Authorization")
+
+ 
+            urlRequest.httpBody = data
+ 
+        } catch {
+            throw AFError.parameterEncodingFailed(reason: .jsonEncodingFailed(error: error))
+        }
+ 
+        return urlRequest
+    }
 }
