dd4d4de92aca93de44ad68909ad01976be934673
cloud <>
2025-09-16 09:04:33 +0800

    支持打印请求各个阶段的耗时时间

--- SwiftProject/Universal/http/HttpManager+Respone.swift
+++ SwiftProject/Universal/http/HttpManager+Respone.swift
@@ -18,9 +18,40 @@ extension Alamofire.DataRequest{
     }
     private func respose(_ serializer:DataResponseSerializer,success:((_ value:AnyObject) -> Void)?,failure: FailureHandler?){
         response(responseSerializer: serializer) { (response) in
+            
+            let metrics = response.metrics
+            for transactionMetric in metrics?.transactionMetrics ?? [] {
+                // 打印各个阶段的耗时
+                if let fetchStartDate = transactionMetric.fetchStartDate, // 开始请求时间
+                   let domainLookupStartDate = transactionMetric.domainLookupStartDate, // DNS 解析开始时间
+                   let domainLookupEndDate = transactionMetric.domainLookupEndDate, // DNS 解析结束时间
+                   let connectStartDate = transactionMetric.connectStartDate, // TCP 连接开始时间
+                   let connectEndDate = transactionMetric.connectEndDate, // TCP 连接结束时间
+                   let secureConnectionStartDate = transactionMetric.secureConnectionStartDate, // TLS 握手开始时间
+                   let secureConnectionEndDate = transactionMetric.secureConnectionEndDate, // TLS 握手结束时间
+                   let requestStartDate = transactionMetric.requestStartDate,  // 请求发送开始时间
+                   let requestEndDate = transactionMetric.requestEndDate, // 请求发送结束时间
+                   let responseStartDate = transactionMetric.responseStartDate, // 服务器处理开始时间
+                   let responseEndDate = transactionMetric.responseEndDate { // 服务器处理结束时间
+                    
+                    let dnsTime = domainLookupEndDate.timeIntervalSince(domainLookupStartDate)
+                    let connectTime = connectEndDate.timeIntervalSince(connectStartDate)
+                    let tlsTime = secureConnectionEndDate.timeIntervalSince(secureConnectionStartDate)
+                    let requestTime = requestEndDate.timeIntervalSince(requestStartDate)
+                    let serverProcessingTime = responseStartDate.timeIntervalSince(requestEndDate)
+                    let responseTime = responseEndDate.timeIntervalSince(responseStartDate)
+                    
+                    print("DNS 解析耗时: \(dnsTime) seconds")
+                    print("TCP 连接时间: \(connectTime) seconds")
+                    print("TLS 握手时间: \(tlsTime) seconds")
+                    print("请求发送时间: \(requestTime) seconds")
+                    print("服务端处理时间: \(serverProcessingTime) seconds")
+                    print("服务器响应时间: \(responseTime) seconds")
+                }
+            }
             print("request = \(String(describing:response.request?.url?.absoluteString))\nresponse = \(String(describing: response.request?.allHTTPHeaderFields))\n,data = \(String(describing: response.data?.dataToJSON()))")
             switch response.result{
-            case .success(let value):
+            case .success(_):
                 self.vaildResponseData(data: response.data, success: success,failure: failure)
             case .failure(let error):
                 failure?(error as Error)
