c9f83e377d8154d45dfcc358eb811b8e28dcb579
cloud <>
2025-12-16 16:31:09 +0800

    兼容iOS 13 使用 SceneDelegate

--- SwiftProject/AppDelegate.swift
+++ SwiftProject/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFAudio
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,35 +21,41 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        self.window = UIWindow(frame: UIScreen.main.bounds)
-        self.window?.backgroundColor = UIColor.clear
-      
-        self.window!.rootViewController = self.tabbarController
-
-        self.window?.makeKeyAndVisible()
-       
-        //启动程序时未登录时 选择 push 跳转登录 退出登录以及挤掉账号时 模态present 跳转到登录
-        if(S_UserInfoLocal.isLogin()){
-           
-
-        }else{
-//            UIViewController.current().navigationController?.pushViewController(S_LoginViewController(), animated: true)
-        }
-
-//        let splashView = S_SplashView.init(frame: self.window!.bounds)
-//        self.window!.addSubview(splashView)
+//        self.window = UIWindow(frame: UIScreen.main.bounds)
+//        self.window?.backgroundColor = UIColor.clear
+//      
+//        self.window!.rootViewController = self.tabbarController
 //
-//        if ((UserDefaults.standard.object(forKey: FirstAgreementKey) == nil)) {
-//            //同意隐私协议
-//            self.window?.insertSubview(self.privacyAgreementView, at: 1)
-////            let appGuideView = S_AppGuideView.init(frame: self.window!.bounds)
-////            self.window?.insertSubview(appGuideView, at: 1)
+//        self.window?.makeKeyAndVisible()
+//       
+//        //启动程序时未登录时 选择 push 跳转登录 退出登录以及挤掉账号时 模态present 跳转到登录
+//        if(S_UserInfoLocal.isLogin()){
+//           
+//
+//        }else{
+////            UIViewController.current().navigationController?.pushViewController(S_LoginViewController(), animated: true)
 //        }
-        
+//
+////        let splashView = S_SplashView.init(frame: self.window!.bounds)
+////        self.window!.addSubview(splashView)
+////
+////        if ((UserDefaults.standard.object(forKey: FirstAgreementKey) == nil)) {
+////            //同意隐私协议
+////            self.window?.insertSubview(self.privacyAgreementView, at: 1)
+//////            let appGuideView = S_AppGuideView.init(frame: self.window!.bounds)
+//////            self.window?.insertSubview(appGuideView, at: 1)
+////        }
+//        
+//        let audioSession = AVAudioSession.sharedInstance()
+//        do {
+//            try audioSession.setCategory(.playAndRecord,mode: .videoRecording, options: [.defaultToSpeaker,.allowBluetoothHFP])
+//            try audioSession.setActive(true)
+//        } catch {
+//
+//        }
 
         return true
     }
-
 
 
 }
